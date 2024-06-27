652ca3a62cc08686587305e452ab92cfb860736f
Saurabh Davda <saurabhdavda9977@gmail.com>
2024-06-27 11:20:09 +0530

    commit fn works

--- Rocket.mjs
+++ Rocket.mjs
@@ -34,17 +34,46 @@ class Rocket{
         console.log(fileHash);
         const newFileHashedobjectPath = path.join(this.objectsPath,fileHash); // .rocket/objects/xyz123
         await fs.writeFile(newFileHashedobjectPath,fileData);
-        await this.updateStagingArea(filePath,fileHash);
+        await this.updateStagingArea(fileToBeAdded,fileHash);
         console.log(`Added ${fileToBeAdded} `);
     }
 
     async updateStagingArea(filePath,fileHash){
-        const index = JSON.parse(await fs.readFile(this.indexPath,{encoding:'utf-8'}));
-
+        const index = JSON.parse(await fs.readFile(this.indexPath,{encoding:'utf-8'}));//read the index file
         index.push({path:filePath,hash:fileHash});
         await fs.writeFile(this.indexPath,JSON.stringify(index));
     }
+
+    async commit(message){
+        const index = JSON.parse(await fs.readFile(this.indexPath,{encoding:'utf-8'}));
+        const parentCommit = await this.getCurrentHead();
+
+        const commitData = {
+            timeStamp: new Date().toISOString(),
+            message,
+            files:index,
+            parent:parentCommit
+        }
+
+        const commitHash = this.hashObject(JSON.stringify(commitData));
+        const commitPath = path.join(this.objectsPath,commitHash);
+        await fs.writeFile(commitPath,JSON.stringify(commitData));
+        await fs.writeFile(this.headPath,commitHash);//updating head to point to the new commit
+        await fs.writeFile(this.indexPath,JSON.stringify([]));//clear the staging area
+        console.log(`Commit successfull : ${commitHash}`);
+    }
+
+    async getCurrentHead(){
+        try{
+            return await fs.readFile(this.headPath,{encoding:'utf-8'});
+        }catch(error){
+            return null;
+        }   
+    }
 }
 
-const rocket = new Rocket();
-rocket.add('sample.txt');
+(async()=>{
+    const rocket = new Rocket();
+    await rocket.add('sample.txt');
+    await rocket.commit('initial commit');
+})();   
